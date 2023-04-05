b4624baf7754797fbca1b1f3c258f5b9e1e03b60
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-05 14:58:50 +0200

    Snack 1

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Snack1 
+
+int num1;
+int num2;
+
+Console.WriteLine("Inserisci primo numero...");
+
+while (!int.TryParse(Console.ReadLine(), out num1)){
+    Console.WriteLine("Non hai inserito un numero, riprova");
+}
+
+Console.WriteLine("Inserisci secondo numero...");
+
+while (!int.TryParse(Console.ReadLine(), out num2))
+{
+    Console.WriteLine("Non hai inserito un numero, riprova");
+}
+
+if (num1 > num2)
+    Console.WriteLine("Il numero più grande è: " + num1);
+else if (num1 < num2)
+    Console.WriteLine("Il numero più grande è: " + num2);
+else
+    Console.WriteLine("I numeri sono uguali!");
+
